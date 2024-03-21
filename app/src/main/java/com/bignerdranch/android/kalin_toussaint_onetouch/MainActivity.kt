20303f31df6c1c7b066f4568c5699ff98ef45755
kalin1233 <kalin32@bu.edu>
2024-03-21 17:05:27 -0400

    Done

--- app/src/main/java/com/bignerdranch/android/kalin_toussaint_onetouch/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/kalin_toussaint_onetouch/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.bignerdranch.android.kalin_toussaint_onetouch
 
 import android.content.Context
-import android.content.SharedPreferences
-import android.net.http.SslCertificate.saveState
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,21 +18,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.edit
 import kotlin.random.Random
 
 
@@ -45,7 +38,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val context = LocalContext.current
-            val (imageIndex, setImageIndex) = remember { mutableStateOf(0) }
+            val (imageIndex, setImageIndex) = remember { mutableIntStateOf(0) }
             val (imageText, setImageText) = remember { mutableStateOf("") }
 
             // Retrieve stored values from SharedPreferences
